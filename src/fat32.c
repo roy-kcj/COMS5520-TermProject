443bca876f91b0ec2adcf89129a9d94769e629ce
Mustafa Khan <149816239+MustafaKhan77790@users.noreply.github.com>
2024-11-30 22:06:11 -0600

    Update fat32.c

--- src/fat32.c
+++ src/fat32.c
@@ -1,17 +1,26 @@
+// fat32.c
 #include "fat32.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-// Simplified FAT32 insert function
-void fat32_insert(const char *filename, const char *data) {
-    write_to_file(filename, data);
+static BPTreeNode* create_node(bool is_leaf) {
+    BPTreeNode* node = (BPTreeNode*)malloc(sizeof(BPTreeNode));
+    node->is_leaf = is_leaf;
+    node->key_count = 0;
+    node->next = NULL;
+    memset(node->keys, 0, sizeof(node->keys));
+    memset(node->children, 0, sizeof(node->children));
+    memset(node->entries, 0, sizeof(node->entries));
+    return node;
 }
 
-void fat32_delete(const char *filename) {
-    remove(filename);
-}
-
-char* fat32_search(const char *filename) {
-    return read_from_file(filename);
+static uint32_t allocate_cluster(FAT32_FileSystem* fs) {
+    for (uint32_t i = 0; i < fs->total_clusters; i++) {
+        if (!(fs->bitmap[i / 8] & (1 << (i % 8)))) {
+            fs->bitmap[i / 8] |= (1 << (i % 8));
+            return i;
+        }
+    }
+    return 0xFFFFFFFF;
 }
