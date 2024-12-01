a2e928fcb63971ed09974c7a1ba395da94345d5f
Mustafa Khan <149816239+MustafaKhan77790@users.noreply.github.com>
2024-12-01 15:24:17 -0600

    Update fat32.h

--- src/fat32.h
+++ src/fat32.h
@@ -1,45 +1,62 @@
-// fat32.h
 #ifndef FAT32_H
 #define FAT32_H
 
 #include <stdint.h>
-#include <stdbool.h>
 #include <time.h>
 
+// FAT32 constants
+#define SECTOR_SIZE 512
 #define CLUSTER_SIZE 4096
 #define MAX_FILENAME 256
-#define B_TREE_ORDER 4
+#define FAT_ENTRY_SIZE 32
 
+// File attributes
+#define ATTR_READ_ONLY 0x01
+#define ATTR_HIDDEN    0x02
+#define ATTR_SYSTEM    0x04
+#define ATTR_VOLUME_ID 0x08
+#define ATTR_DIRECTORY 0x10
+#define ATTR_ARCHIVE   0x20
+
+// FAT32 entry structure
 typedef struct {
     char filename[MAX_FILENAME];
-    uint32_t start_cluster;
-    uint32_t file_size;
+    uint32_t fileSize;
+    uint32_t startCluster;
     uint8_t attributes;
-    time_t created_time;
-    time_t modified_time;
+    time_t creationTime;
+    time_t modificationTime;
+    uint32_t bitmapAddress;
 } FAT32_Entry;
 
-typedef struct BPTreeNode {
-    bool is_leaf;
-    int key_count;
-    char keys[B_TREE_ORDER][MAX_FILENAME];
-    struct BPTreeNode* children[B_TREE_ORDER + 1];
-    FAT32_Entry* entries[B_TREE_ORDER];
-    struct BPTreeNode* next;
-} BPTreeNode;
-
+// FAT32 file system structure
 typedef struct {
-    uint32_t total_clusters;
-    uint32_t* fat_table;
+    uint32_t totalSectors;
+    uint32_t sectorsPerCluster;
+    uint32_t reservedSectors;
+    uint32_t numberOfFATs;
+    uint32_t sectorsPerFAT;
+    uint32_t rootCluster;
+    uint8_t* fatTable;
+    uint8_t* data;
+    uint32_t dataSize;
     uint8_t* bitmap;
-    BPTreeNode* root;
+    uint32_t bitmapSize;
 } FAT32_FileSystem;
 
-// Function declarations
+// Core function declarations
 FAT32_FileSystem* fat32_init(uint32_t size);
-void fat32_insert(FAT32_FileSystem* fs, const char* filename, const char* data);
-void fat32_delete(FAT32_FileSystem* fs, const char* filename);
-char* fat32_search(FAT32_FileSystem* fs, const char* filename);
+FAT32_Entry* create_file_entry(FAT32_FileSystem* fs, const char* filename, uint32_t size);
+bool fat32_write(FAT32_FileSystem* fs, FAT32_Entry* entry, const void* data, uint32_t size);
+void* fat32_read(FAT32_FileSystem* fs, FAT32_Entry* entry);
+bool fat32_delete(FAT32_FileSystem* fs, FAT32_Entry* entry);
 void fat32_cleanup(FAT32_FileSystem* fs);
+
+// Helper function declarations
+uint32_t allocate_clusters(FAT32_FileSystem* fs, uint32_t count);
+void free_clusters(FAT32_FileSystem* fs, uint32_t startCluster);
+uint32_t get_next_cluster(FAT32_FileSystem* fs, uint32_t cluster);
+void set_next_cluster(FAT32_FileSystem* fs, uint32_t cluster, uint32_t next);
+uint32_t cluster_to_sector(FAT32_FileSystem* fs, uint32_t cluster);
 
 #endif
